aba12c5e013f5dc0000e2e6f41eca51f4a2e66c1
Akanksharma00 <akanksha.uieth@yahoo.com>
2026-03-15 12:05:21 +0530

    feat: added pdf to db code

--- app/dashboard/_components/UploadPdfDialog.tsx
+++ app/dashboard/_components/UploadPdfDialog.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import { Button } from "@/components/ui/button"
 import {
   Dialog,
@@ -10,9 +12,61 @@ import {
   DialogTrigger,
 } from "@/components/ui/dialog"
 import { Input } from "@/components/ui/input"
-import { ReactNode } from "react"
+import { api } from "@/convex/_generated/api"
+import { useUser } from "@clerk/nextjs"
+import { useMutation } from "convex/react"
+import { Loader2Icon } from "lucide-react"
+import { ChangeEvent, ReactNode, useState } from "react"
+import { v4 as uuid4 } from "uuid"
 
 const UploadPdfDialog = ({children}: {children: ReactNode}) => {
+    const [file, setFile] = useState<File | null>(null);
+    const [isLoading, setIsLoading] = useState<boolean>(false);
+    const [fileName, setFileName] = useState<string>('');
+
+    const {user} = useUser();
+
+    const generateUploadUrl = useMutation(api.fileStorage.generateUploadUrl);
+    const addFileEntryToDb = useMutation(api.fileStorage.addFileEntryToDb);
+    const getFileUrl = useMutation(api.fileStorage.getFileUrl);
+
+    const onFileSelect = (e: ChangeEvent<HTMLInputElement, HTMLInputElement>) => {
+        setFile(e.target.files?.[0] || null);
+    }
+
+    const handleUpload = async () => {
+        setIsLoading(true);
+
+        // Step 1 : Generate a short lived upload URL
+        const uploadUrl = await generateUploadUrl();
+
+        // Step 2: POST the file to the URL
+        const result = await fetch(uploadUrl, {
+            method: "POST",
+            headers: { "Content-Type": file!.type },
+            body: file,
+        });
+        const { storageId } = await result.json();
+
+        const fileId = uuid4();
+
+        const fileUrl = await getFileUrl({storageId: storageId});
+
+        // Step 3: Save the newly allocated storage id to the database
+        const dbResult = await addFileEntryToDb({
+            fileId: fileId,
+            storageId:storageId,
+            fileName: fileName || 'Untitled PDF',
+            fileUrl: fileUrl || '',
+            createdBy: user?.primaryEmailAddress?.emailAddress || '',
+
+        });
+
+        console.log("Upload URL ::: ", dbResult);
+
+        setIsLoading(false);
+    }
+
   return (
     <Dialog>
     <DialogTrigger asChild>{children}</DialogTrigger>
@@ -23,11 +77,20 @@ const UploadPdfDialog = ({children}: {children: ReactNode}) => {
             <div>
                 <h2 className="mt-2">Select file to upload</h2>
                 <div className="border p-1 rounded-md">
-                    <input type="file" accept="application/pdf" className="pl-2 hover:border cursor-pointer"/>
+                    <input 
+                        type="file" 
+                        accept="application/pdf" 
+                        className="pl-2 hover:border cursor-pointer"
+                        onChange={(e) => onFileSelect(e)}
+                    />
                 </div>
                 <div className="mt-2">
                     <label>File Name *</label>
-                    <Input placeholder="Enter file name"/>
+                    <Input 
+                        placeholder="Enter file name"
+                        value={fileName}
+                        onChange={(e) => setFileName(e.target.value)}
+                    />
                 </div>
             </div>
         </DialogDescription>
@@ -36,7 +99,12 @@ const UploadPdfDialog = ({children}: {children: ReactNode}) => {
           <DialogClose asChild>
             <Button variant="outline">Close</Button>
           </DialogClose>
-            <Button variant="default">Upload</Button>
+            <Button 
+                variant="default"
+                onClick={handleUpload}
+            >
+                {isLoading ? <Loader2Icon className="animate-spin" /> : 'Upload'}
+            </Button>
         </DialogFooter>
     </DialogContent>
     </Dialog>
@@ -44,3 +112,4 @@ const UploadPdfDialog = ({children}: {children: ReactNode}) => {
 }
 
 export default UploadPdfDialog
+
---
+++ convex/fileStorage.ts
@@ -0,0 +1,40 @@
+import { v } from "convex/values";
+import { mutation } from "./_generated/server";
+
+export const generateUploadUrl = mutation({
+  args: {},
+  handler: async (ctx) => {
+    return await ctx.storage.generateUploadUrl();
+  },
+});
+
+export const addFileEntryToDb = mutation({
+  args: {
+    fileId: v.string(),
+    storageId: v.string(),
+    fileName: v.string(),
+    fileUrl: v.string(),
+    createdBy: v.string(),
+  },
+  handler: async (ctx, args) => {
+    const result = await ctx.db.insert("pdfFiles", {
+      fileId: args.fileId,
+      storageId: args.storageId,
+      fileName: args.fileName,
+      fileUrl: args.fileUrl,
+      createdBy: args.createdBy,
+    });
+
+    return 'File entry added with id: ' + result;
+  }
+});
+
+export const getFileUrl = mutation({
+  args:{
+    storageId: v.string(),
+  },
+  handler: async (ctx, args) => {
+    const url = await ctx.storage.getUrl(args.storageId);
+    return url;
+  }
+});
